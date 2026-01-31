213e538080ef494ad034c9a42706b344cb4e2cf9
Kobbycode <alexco.ao@gmail.com>
2026-01-31 10:04:31 +0000

    Test firebase initialization in diagnostic endpoint

--- api/diag.ts
+++ api/diag.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { getAdminApp } from './_firebaseAdmin';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     const saRaw = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
@@ -15,11 +16,22 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
         }
     }
 
+    let adminInitialized = false;
+    let adminInitError = '';
+    try {
+        const app = getAdminApp();
+        adminInitialized = !!app;
+    } catch (e: any) {
+        adminInitError = e.message;
+    }
+
     res.status(200).json({
         hasServiceAccount: !!saRaw,
         serviceAccountLength: saRaw ? saRaw.length : 0,
         saParseable,
         saError,
+        adminInitialized,
+        adminInitError,
         hasBootstrapSecret: !!secret,
         nodeVersion: process.version,
         platform: process.platform,
